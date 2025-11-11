ff6b701e35ed9664d8dee554b3b33c27d01cd08c
Samuel Shonde <samuelshonde07@gmail.com>
2025-11-11 14:11:24 +0100

    updated

--- src/components/Hamburger.tsx
+++ src/components/Hamburger.tsx
@@ -1,10 +1,9 @@
 'use client'
 
 import { cn } from '@/lib/utils'
-import { HomeIcon, ShoppingBagIcon, ShoppingBasket, UserCircleIcon } from 'lucide-react'
+import { HomeIcon, ShoppingBagIcon, ShoppingBasket, User2Icon } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import path from 'path'
 
 const Hamburger = () => {
 
@@ -14,16 +13,16 @@ const Hamburger = () => {
     { name: 'Home', href: '/', icon: <HomeIcon size={20} /> },
     { name: 'Shop', href: '/shop', icon: <ShoppingBagIcon size={20} /> },
     { name: 'Cart', href: '/cart', icon: <ShoppingBasket size={20} /> },
-    { name: 'Profile', href: '/profile', icon: <UserCircleIcon size={20} /> },
+    { name: 'Profile', href: '/profile', icon: <User2Icon size={20} /> },
   ]
 
   return (
-    <div className='flex items-center justify-between gap-4 py-2 container md:hidden'>
+    <div className='flex items-center justify-between gap-4 py-2 px-12 container md:hidden'>
       {
         links.map((link, index) => {
           const isActive = pathname === link.href;
           return (
-            <Link key={index} href={link.href} className={cn('flex flex-col items-center text-sm', isActive ? 'text-green':'text-neutral-4')}>{link.icon}<span>{link.name}</span></Link>)
+            <Link key={index} href={link.href} className={cn('flex flex-col gap-0.5 items-center text-xs', isActive ? 'text-green':'text-neutral-4')}>{link.icon}<span>{link.name}</span></Link>)
         }
         )
       }
