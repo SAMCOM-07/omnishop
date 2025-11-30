fc9a46289a9609094c0b94470fd429916ab3fabb
Samuel Shonde <samuelshonde07@gmail.com>
2025-11-30 15:19:08 +0100

    refined search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -6,46 +6,36 @@ import { useEffect, useState } from 'react'
 
 const SearchBar = ({ q }: { q: string }) => {
 
-    const router = useRouter()
+  const router = useRouter()
 
-    const [query, setQuery] = useState('')
+  const [query, setQuery] = useState('')
 
-    useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
+  return (
+    <div className='border border-border text-neutral-4 p-3 rounded-full flex items-center gap-3 font-inter'>
+      <Search />
+      <input
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+          setQuery(e.target.value);
 
-        if (!query.trim()) {
-            params.delete("q"); // <-- important fix
-        } else {
-            params.set("q", query);
-        }
+        }}
 
-        const newQuery = params.toString();
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') router.push(`/search?q=${query}`)
+        }}
+        value={query}
+        type="text"
+        placeholder='Search . . .'
+        className='outline-0 grow'
+      />
 
-        router.push(`/search?${newQuery}`);
-    }, [query]);
+      <button onClick={() => {
+        setQuery('');
+        router.push('/search')
+      }}
+        className={`${query ? 'block' : 'hidden'}`}><X size={18} /></button>
 
-
-    return (
-        <div className='border border-border text-neutral-4 p-3 rounded-full flex items-center gap-3 font-inter'>
-            <Search />
-            <input
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                    setQuery(e.target.value);
-                }}
-                value={q ? q : query}
-                type="text"
-                placeholder='Search . . .'
-                className='outline-0 grow'
-            />
-
-            <button onClick={() => {
-                setQuery('');
-                router.push('/search')
-            }}
-                className={`${query ? 'block' : 'hidden'}`}><X size={18} /></button>
-
-        </div>
-    )
+    </div>
+  )
 }
 
 export default SearchBar
