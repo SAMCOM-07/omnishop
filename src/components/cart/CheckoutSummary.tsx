1e9b8682c4557cf59426283b339715c0d348a90c
Samuel Shonde <samuelshonde07@gmail.com>
2025-12-29 20:22:00 +0100

    updated

--- src/components/cart/CheckoutSummary.tsx
+++ src/components/cart/CheckoutSummary.tsx
@@ -16,35 +16,35 @@ const CheckoutSummary = () => {
 
         {/* contact information */}
         <div className='border-card'>
-          <h3 className='text-neutral-7'>Contact Information</h3>
+          <span className='text-neutral-7 text-lg font-semibold'>Contact Information</span>
           <div className='flex items-center gap-4'>
-            <label className='w-full'><span className='font-semibold block'>FIRST NAME *</span><input type="text" placeholder='First name' className="input-field" /></label>
-            <label className='w-full'><span className='font-semibold block'>LAST NAME *</span><input type="text" placeholder='Last name' className="input-field" /></label>
+            <label className='w-full'><span className='font-semibold block'>FIRST NAME *</span><input required type="text" placeholder='First name' className="input-field" /></label>
+            <label className='w-full'><span className='font-semibold block'>LAST NAME *</span><input required type="text" placeholder='Last name' className="input-field" /></label>
           </div>
 
-          <label className='w-full'><span className='font-semibold block'>PHONE NUMBER *</span><input type="tel" placeholder='Phone number' className="input-field" /></label>
-          <label className='w-full'><span className='font-semibold block'>EMAIL *</span><input type="email" placeholder='Email' className="input-field" /></label>
+          <label className='w-full'><span className='font-semibold block'>PHONE NUMBER *</span><input required type="tel" placeholder='Phone number' className="input-field" /></label>
+          <label className='w-full'><span className='font-semibold block'>EMAIL *</span><input required type="email" placeholder='Email' className="input-field" /></label>
         </div>
 
         {/* shipping address */}
         <div className="border-card">
-          <h3 className='text-neutral-7'>Shipping Address</h3>
-          <label className='w-full'><span className='font-semibold block'>STREET ADDRESS *</span><input type="text" placeholder='Street address' className="input-field" /></label>
-          <label className='w-full'><span className='font-semibold block'>COUNTRY *</span><input type="text" placeholder='Country' className="input-field" /></label>
-          <label className='w-full'><span className='font-semibold block'>TOWN / CITY *</span><input type="text" placeholder='Town / City' className="input-field" /></label>
+          <span className='text-neutral-7 text-lg font-semibold'>Shipping Address</span>
+          <label className='w-full'><span className='font-semibold block'>STREET ADDRESS *</span><input required type="text" placeholder='Street address' className="input-field" /></label>
+          <label className='w-full'><span className='font-semibold block'>COUNTRY *</span><input required type="text" placeholder='Country' className="input-field" /></label>
+          <label className='w-full'><span className='font-semibold block'>TOWN / CITY *</span><input required type="text" placeholder='Town / City' className="input-field" /></label>
           <div className='flex items-center gap-4'>
-            <label className='w-full'><span className='font-semibold block'>STATE *</span><input type="text" placeholder='State' className="input-field" /></label>
-            <label className='w-full'><span className='font-semibold block'>ZIP CODE *</span><input type="text" placeholder='Zip code' className="input-field" /></label>
+            <label className='w-full'><span className='font-semibold block'>STATE *</span><input required type="text" placeholder='State' className="input-field" /></label>
+            <label className='w-full'><span className='font-semibold block'>ZIP CODE *</span><input required type="text" placeholder='Zip code' className="input-field" /></label>
           </div>
         </div>
 
         {/* Payment Method */}
         <div className="border-card">
-          <h3 className='text-neutral-7'>Payment Method</h3>
-          <label className='w-full'><span className='font-semibold block'>CARD NUMBER *</span><input type="text" placeholder='0000 0000 0000 0000' className="input-field" /></label>
+          <span className='text-neutral-7 text-lg font-semibold'>Payment Method</span>
+          <label className='w-full'><span className='font-semibold block'>CARD NUMBER *</span><input required type="text" placeholder='0000 0000 0000 0000' className="input-field" /></label>
           <div className='flex items-center gap-4'>
-            <label className='w-full'><span className='font-semibold block'>EXPIRY DATE *</span><input type="text" placeholder='MM/YY' className="input-field" /></label>
-            <label className='w-full'><span className='font-semibold block'>CVV *</span><input type="text" placeholder='CVV' className="input-field" /></label>
+            <label className='w-full'><span className='font-semibold block'>EXPIRY DATE *</span><input required type="text" placeholder='MM/YY' className="input-field" /></label>
+            <label className='w-full'><span className='font-semibold block'>CVV *</span><input required type="text" placeholder='CVV' className="input-field" /></label>
           </div>
         </div>
         <Link href="/completed" className="w-full block bg-neutral-5 text-white p-3 rounded-md hover:bg-neutral-7 transition-all duration-300 text-center mt-4 active:scale-95">Place Order</Link>
